424173628b77dc781a4a4ebde6e7694088860db4
jfmsantana <jfmsantana10@gmail.com>
2023-12-10 20:02:41 -0300

    Atualizado o código para processamento infinito de processos

--- src/fcfs.c
+++ src/fcfs.c
@@ -9,40 +9,43 @@ void fcfs() {
 
     int iteracoesTotal = 0, processosCriados = 0;
     no *f = NULL, *r = NULL;
-    int numeroProcessos = tentarCriarProcesso(processosCriados);
-    if (numeroProcessos == 0) // Se não foi possível criar nenhum processo, encerra a função
+    while (1)
     {
-        printf("Não foi possível criar nenhum processo.\n");
-        return;
-    }
-    // Se chegou aqui, é porque foi possível criar pelo menos um processo
-    for (int i = 0; i < numeroProcessos; i++) // Cria os processos e os insere na fila
-    {
-        inserirFila(&f, &r, alocarNo(&processosCriados));
-    }
-    while (1) // Loop Infinito
-    {
-        no *processoAtual = removerFila(&f, &r);
-        if (processoAtual == NULL) // Se não há mais processos na fila, encerra a função
+        int numeroProcessos = tentarCriarProcesso(processosCriados);
+        if (numeroProcessos == 0) // Se não foi possível criar nenhum processo, aguarde até que algum seja criado.
         {
-            printf("Não há mais processos na fila.\n");
-            return;
+            printf("Não foi possível criar nenhum processo, aguarde até que algum seja criado.\n");
+            continue; // Volta para o início do loop, tentando criar um novo processo novamente.
         }
-        while (processoAtual->tamanho > 0) // Executa o processo (um por vez) até que ele seja concluído
+        // Se chegou aqui, é porque foi possível criar pelo menos um processo
+        for (int i = 0; i < numeroProcessos; i++) // Cria os processos e os insere na fila
         {
-            iteracoesTotal++;
-            processoAtual->tamanho--;
+            inserirFila(&f, &r, alocarNo(&processosCriados));
         }
-        printf("O processo %d foi concluído.\n", processoAtual->id); // Já saiu do while, então o processo foi concluído
-        printf("Iterações totais executadas: %d\n", iteracoesTotal); // Exibe o total de iterações executadas
-        printf("==============================================\n");
+        while (1) // Loop Infinito
+        {
+            no *processoAtual = removerFila(&f, &r);
+            if (processoAtual == NULL) // Se não há mais processos na fila, volta para o início do loop principal
+            {
+                printf("Não há mais processos na fila.\n");
+                break; // Sai do loop interno e volta para o loop principal, tentando criar um novo processo novamente.
+            }
+            while (processoAtual->tamanho > 0) // Executa o processo (um por vez) até que ele seja concluído
+            {
+                iteracoesTotal++;
+                processoAtual->tamanho--;
+            }
+            printf("O processo %d foi concluído.\n", processoAtual->id); // Já saiu do while, então o processo foi concluído
+            printf("Iterações totais executadas: %d\n", iteracoesTotal); // Exibe o total de iterações executadas
+            printf("==============================================\n");
 
-        // Agora, ao final da iteração haverá chance de 30% de criar um novo processo
-        if (tentarCriarProcesso()) // Se conseguiu criar
-        {
-            no *novoProcesso = alocarNo(&processosCriados); // Aloca o novo processo
-            inserirFila(&f, &r, novoProcesso);              // Insere o novo processo na fila
-            printf("O processo %d, com tamanho %d foi criado.\n", novoProcesso->id, novoProcesso->tamanho);
+            // Agora, ao final da iteração haverá chance de 30% de criar um novo processo
+            if (tentarCriarProcesso()) // Se conseguiu criar
+            {
+                no *novoProcesso = alocarNo(&processosCriados); // Aloca o novo processo
+                inserirFila(&f, &r, novoProcesso);              // Insere o novo processo na fila
+                printf("O processo %d, com tamanho %d foi criado.\n", novoProcesso->id, novoProcesso->tamanho);
+            }
         }
     }
-}git add src/fcfs.c
+}
