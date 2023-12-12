37a91d3f087fbffee400e6d2dc609f40eb5d3c22
jfmsantana <jfmsantana10@gmail.com>
2023-12-12 15:34:35 -0300

    Versão Final FCFS (com cores e "sleep(1)"

--- src/fcfs.c
+++ src/fcfs.c
@@ -16,25 +16,25 @@ void fcfs()
             no *processoAtual = alocarNo(&processosCriados); // Aloca um novo nó
             inserirFila(&f, &r, processoAtual);              // Insere o nó na fila por meio da função inserirFila
 
-            printf("Processo %d criado com tamanho %d\n", processoAtual->id, processoAtual->tamanho);
-
-            if (f != NULL) // Se o processo ainda não tiver acabado, f (primeiro da fila) não será NULL
+            printf("\033[0;34mProcesso %d de tamanho %d criado!\033[0m\n", processoAtual->id, processoAtual->tamanho);
+        }
+    
+        if (f != NULL) // Se o processo ainda não tiver acabado, f (primeiro da fila) não será NULL
+        {
+            printf("Processo %d executando...\n", f->id);
+            printf("Tamanho restante: %d\n", f->tamanho);
+            sleep(1);
+            f->tamanho--;        // Diminui o tamanho do processo em uma iteração, já que cada loop do while é uma iteração
+            if (f->tamanho == 0) // Se o processo acabou (ou quando o processo acabar)
             {
-                printf("O processo %d está em execução com tamanho %d\n", f->id, f->tamanho);
-                sleep(5);
-                f->tamanho--; // Diminui o tamanho do processo em uma iteração, já que cada loop do while é uma iteração
-
-                if (f->tamanho == 0) // Se o processo acabou (ou quando o processo acabar)
-                {
-                    printf("O processo %d foi finalizado\n", f->id);
-                    removerFila(&f, &r); // Remove o processo da fila por meio da função removerFila
-                }
+                printf("\033[0;32mProcesso %d finalizado!\033[0m\n", f->id);
+                removerFila(&f, &r); // Remove o processo da fila por meio da função removerFila
             }
-            else
-            {
-                printf("Nenhum processo criado, aguarde até que outro processo seja criado\n");
-                sleep(5);
-            }
+        }
+        else
+        {
+            printf("Nenhum processo criado, aguarde até que outro processo seja criado\n");
+            sleep(1);
         }
     }
 }
